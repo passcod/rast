8d7c62d957e51b7ec1a69bad21665405804bf764
Félix Saparelli <felix@passcod.name>
2014-05-12 03:01:59 +1200

    Add streams

--- rast.rs
+++ rast.rs
@@ -3,13 +3,14 @@
 
 extern crate collections;
 use collections::HashMap;
+use std::io::{Reader, Writer};
 
 trait Handler {
   fn run(app: &App);
 }
 
 trait App {
-  fn call(environment: HashMap<&str, &str>) -> Response;
+  fn call(environment: HashMap<&str, &str>, input: &Reader, error: &Writer) -> Response;
 }
 
 struct Response<'a> {
