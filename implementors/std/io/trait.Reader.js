cbb33ccc8800bdecb9bac4c264087b0aadff8a2b
Félix Saparelli <felix@passcod.name>
2014-06-10 23:01:42 +1200

    MemStream Tue Jun 10 23:01:42 NZST 2014

---
+++ implementors/std/io/trait.Reader.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['memstream'] = ["impl <a class='trait' href='http://doc.rust-lang.org/std/io/trait.Reader.html' title='std::io::Reader'>Reader</a> for <a class='struct' href='memstream/struct.DListStream.html' title='memstream::DListStream'>DListStream</a>","impl <a class='trait' href='http://doc.rust-lang.org/std/io/trait.Reader.html' title='std::io::Reader'>Reader</a> for <a class='struct' href='memstream/struct.RingBufStream.html' title='memstream::RingBufStream'>RingBufStream</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
