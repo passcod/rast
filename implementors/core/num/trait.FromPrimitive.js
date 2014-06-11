b0a7a3a01cb6e96a93da9a4b6e92e5f6b23e891e
Félix Saparelli <felix@passcod.name>
2014-06-11 19:44:12 +1200

    Docrun Wed Jun 11 19:44:12 NZST 2014

---
+++ implementors/core/num/trait.FromPrimitive.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rast'] = ["impl <a class='trait' href='http://doc.rust-lang.org/core/num/trait.FromPrimitive.html' title='core::num::FromPrimitive'>FromPrimitive</a> for <a class='type' href='rast/http/type.Method.html' title='rast::http::Method'>Method</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- implementors/std/io/trait.Reader.js
+++ implementors/std/io/trait.Reader.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors['rast'] = ["impl <a class='trait' href='http://doc.rust-lang.org/std/io/trait.Reader.html' title='std::io::Reader'>Reader</a> for <a class='struct' href='rast/memstream/struct.DListStream.html' title='rast::memstream::DListStream'>DListStream</a>","impl <a class='trait' href='http://doc.rust-lang.org/std/io/trait.Reader.html' title='std::io::Reader'>Reader</a> for <a class='struct' href='rast/memstream/struct.RingBufStream.html' title='rast::memstream::RingBufStream'>RingBufStream</a>",];
 implementors['memstream'] = ["impl <a class='trait' href='http://doc.rust-lang.org/std/io/trait.Reader.html' title='std::io::Reader'>Reader</a> for <a class='struct' href='memstream/struct.DListStream.html' title='memstream::DListStream'>DListStream</a>","impl <a class='trait' href='http://doc.rust-lang.org/std/io/trait.Reader.html' title='std::io::Reader'>Reader</a> for <a class='struct' href='memstream/struct.RingBufStream.html' title='memstream::RingBufStream'>RingBufStream</a>",];
 
             if (window.register_implementors) {
